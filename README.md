611f9fe2b4f77406eaf6c332b41ff9179dd87c73
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2014-03-25 18:28:58 -0300

    Initial version

---
+++ README.md
@@ -0,0 +1,30 @@
+# import_pacman_to_mongo.py
+
+`import_pacman_to_mongo` is a Python 3 script that will gather information about
+your local pacman database (using the pacman executable), convert the
+information to a nice and queryable format and upload it to MongoDB
+(`{"db": "pacman", "collection": "packages"}`).
+
+# Dependencies
+
+The script dependencies:
+
+* Python 3.
+* [python-dateutil](http://labix.org/python-dateutil).
+* [python-pymongo](https://pypi.python.org/pypi/pymongo/).
+* `en_US.UTF-8` must be installed, but it doesn't need to be the default locale.
+* The locale you use to run the script within must be any `UTF-8`-based locale.
+
+# ROADMAP
+
+* Parse command-line arguments.
+  * Support to different MongoDB database IDs.
+  * Support to different MongoDB collection IDs.
+  * Output data to JSON. Dates must be handled correctly.
+* Define a JSON schema to help dude to understand what fields dude should expect
+  in the documents.
+* Rename `name` to `_id` (?).
+
+# LICENSE
+
+The script is lincensed under the MIT license. I don't care about its use.
---
+++ import_pacman_to_mongo.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+#  Copyright (c) 2014 Vinícius dos Santos Oliveira
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
+import os
+import subprocess
+import dateutil.parser
+
+import pymongo
+
+for localekey in ('LANG', 'LANGUAGE', 'LC_CTYPE', 'LC_NUMERIC', 'LC_TIME',
+                  'LC_COLLATE', 'LC_MONETARY', 'LC_MESSAGES', 'LC_PAPER',
+                  'LC_NAME', 'LC_ADDRESS', 'LC_TELEPHONE', 'LC_MEASUREMENT',
+                  'LC_IDENTIFICATION'):
+    os.environ[localekey] = 'en_US.UTF-8'
+
+def identity(x):
+    return x
+
+def provides_value_to_hierarchical(value):
+    try:
+        offset = value.index('=')
+        return {'name': value[:offset], 'version': value[offset+1:]}
+    except ValueError:
+        return {'name': value}
+
+def depends_on_value_to_hierarchical(value):
+    comparisons = ['>=', '<=', '=', '>', '<']
+
+    for c in comparisons:
+        try:
+            offset = value.index(c)
+            return {'name': value[:offset],
+                    'version': {'comparison': c,
+                                'value': value[offset+len(c):]}}
+        except ValueError:
+            pass
+
+    return {'name': value}
+
+def optional_deps_to_hierarchical(buffer):
+    if buffer[0].endswith('None'):
+        return None
+
+    delim = ': '
+    offset = buffer[0].index(delim) + len(delim)
+
+    for i in range(len(buffer)):
+        buffer[i] = buffer[i][offset:]
+
+    extra_info = ' [installed]'
+    for i in range(len(buffer)):
+        if buffer[i].endswith(extra_info):
+            buffer[i] = buffer[i][:-len(extra_info)]
+
+    ret = []
+
+    for spec in buffer:
+        try:
+            offset = spec.index(delim)
+            ret.append({'name': spec[:offset],
+                        'reason': spec[offset+len(delim):]})
+        except ValueError:
+            ret.append({'name': spec})
+
+    return ret
+
+def package_to_hierarchical(package):
+    delim = ': '
+
+    # optional_deps is handled separately
+    multi_valued_properties = {'licenses': identity,
+                               'groups': identity,
+                               'provides': provides_value_to_hierarchical,
+                               'depends_on': depends_on_value_to_hierarchical,
+                               'required_by': identity,
+                               'optional_for': identity,
+                               'conflicts_with': depends_on_value_to_hierarchical,
+                               'replaces': depends_on_value_to_hierarchical}
+    special_properties = {'build_date': dateutil.parser.parse,
+                          'install_date': dateutil.parser.parse}
+
+    ret = {}
+    output = subprocess.check_output(
+        ['pacman', '-Qi', package], universal_newlines=True).split('\n')[:-2]
+    offset = output[0].index(delim) + len(delim)
+
+    # begin of 'Optional Deps' handling
+
+    nextfield = 10
+    while output[nextfield][offset - len(delim)] != delim[0]:
+        nextfield += 1
+
+    optional_deps = optional_deps_to_hierarchical(list(output[9:nextfield]))
+    if optional_deps:
+        ret['optional_deps'] = optional_deps
+    del output[9:nextfield]
+
+    # end of 'Optional Deps' handling
+
+    for line in output:
+        value = line[offset:]
+
+        if value == 'None':
+            continue
+
+        key = line.split(delim)[0].strip().lower().replace(' ', '_')
+        if key not in multi_valued_properties.keys():
+            ret[key] = value if key not in special_properties else (
+                special_properties[key](value))
+        else:
+            ret[key] = [multi_valued_properties[key](i)
+                        for i in value.split('  ')]
+
+    return ret
+
+packages = [i.split(' ')[0]
+            for i in str(subprocess.check_output(['pacman', '-Q'],
+                                                 universal_newlines=True))
+                     .split('\n')[:-1]]
+
+client = pymongo.MongoClient()
+
+# TODO: allow user to specify database and collection
+db = client.pacman
+coll = db.packages
+
+nimported = 0
+for p in packages:
+    coll.insert(package_to_hierarchical(p))
+    nimported += 1
+    print("Imported:", str(nimported) + '/' + str(len(packages)))
